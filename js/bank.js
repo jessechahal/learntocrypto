ec7a777461bb227490cdbbb6bad58e945a799c76
Jesse Chahal <jesse@jessechahal.com>
2018-11-03 19:47:39 -0700

    Finished step 07? I didn't follow recommended way since i didn't understand the hint

--- js/bank.js
+++ js/bank.js
@@ -6,47 +6,99 @@
 var jsonStream = require('duplex-json-stream');
 var net = require('net');
 var fs = require('fs');
+var log4js = require('log4js');
+
+log4js.configure({
+    appenders: { out: { type: 'stdout', layout: { type: 'colored' } } },
+    categories: { default: { appenders: ['out'], level: 'info' } }
+});
+var log = log4js.getLogger();
+log.level = 'info'; // default level is OFF - which means no logs at all.
+
 
 var transactionLogFilename = "./bank_transactionLogs.json";
 var transactionLog = [];
 
 function reduce(someArray) {
     var totalBalance = 0;
-    console.log(someArray);
+    log.debug("",someArray);
     for(let transaction of someArray) {
-        if (transaction.cmd == 'deposit') {
-            totalBalance += parseInt(transaction.amount, 10);
+        if (transaction.value.cmd == 'deposit') {
+            totalBalance += parseInt(transaction.value.amount, 10);
         } else {
-            totalBalance -= parseInt(transaction.amount, 10);
+            var tempBalance = totalBalance;
+            tempBalance -= parseInt(transaction.value.amount, 10);
+
+            if(tempBalance >= 0) {
+                totalBalance = tempBalance;
+            } else {
+                console.log("Withdrew too much money, cannot process transaction")
+            }
         }
     }
     //transactionLog.forEach(function(x) { x['cmd'] == 'deposit' ? totalBalance += x['amount'] : totalBalance -= x['amount']});
     return totalBalance;
-};
+}
 
-function writeTransactionLog() {
+var Web3 = require('web3');
+var web3 = new Web3();
+web3.setProvider(new web3.providers.HttpProvider());
+function hashToHex(valueToHash) {
+    "use strict";
+    return web3.eth.accounts.hashMessage(valueToHash);
+}
+
+// One edge-case with referring to the previous hash is that you need a
+// "genesis" hash for the first entry in the log
+var genesisHash = "0x" + Buffer.alloc(32).toString('hex');
+
+function appendToTransactionLog (transLog, entry) {
+    var prevHash = transLog.length ? transLog[transLog.length - 1].hash : genesisHash;
+    log.debug("prevHash: " + prevHash);
+    transLog.push({
+        value: entry,
+        hash: hashToHex(prevHash + JSON.stringify(entry))
+    })
+}
+
+function writeTransactionLogFile() {
     "use strict";
 
     fs.writeFile(transactionLogFilename, JSON.stringify(transactionLog, null, 2), function (err) {
         if (err) {
-            console.log('saving file failed');
+            log.error('saving file failed');
             throw err;
         }
-        console.log('Transaction logs saved to: ' + transactionLogFilename);
+        log.info('Transaction logs saved to: ' + transactionLogFilename);
     });
 };
 
-try {
-    transactionLog = require(transactionLogFilename);
-    console.log(transactionLogFilename + " was FOUND, loading transaction log from file")
-} catch (exception) {
-    console.log(transactionLogFilename + " file was NOT FOUND, starting from scratch")
+function verifyLog() {
+    newLog = [];
+
+    for(let entry of transactionLog) {
+        appendToTransactionLog(newLog, entry.value);
+    }
+    return JSON.stringify(newLog) == JSON.stringify(transactionLog);
 }
+
+function readTransactionLogFile() {
+    "use strict";
+
+    try {
+        return require(transactionLogFilename);
+        log.info(transactionLogFilename + " was FOUND, reading transaction logs from file");
+    } catch (exception) {
+        log.info(transactionLogFilename + " file was NOT FOUND, starting from scratch");
+
+        return [];
+    }
+};
 
 var server = net.createServer(function (socket) {
     socket = jsonStream(socket);
     socket.on('data', function (msg) {
-        console.log('Bank received:', msg);
+        log.info('Bank received:', msg);
 
         switch (msg['cmd']) {
             case 'balance':
@@ -55,14 +107,14 @@ var server = net.createServer(function (socket) {
                 break;
 
             case 'deposit':
-                transactionLog.push(msg);
-                writeTransactionLog();
+                appendToTransactionLog(transactionLog, msg);
+                writeTransactionLogFile();
                 socket.write({cmd: 'deposit', balance: reduce(transactionLog)});
                 break;
 
             case 'withdraw':
-                transactionLog.push(msg);
-                writeTransactionLog();
+                appendToTransactionLog(transactionLog, msg);
+                writeTransactionLogFile();
                 socket.write({cmd: 'withdraw', balance: reduce(transactionLog)});
                 break;
 
@@ -72,8 +124,14 @@ var server = net.createServer(function (socket) {
                 break;
         }
 
-
     })
 });
+
+transactionLog = readTransactionLogFile();
+if(transactionLog.length > 0 && !verifyLog()) {
+    log.warn("transaction log file has been modified, ignoring file ");
+    log.debug(transactionLog);
+    transactionLog = []
+}
 
 server.listen(3876);
--- js/hash-example.js
+++ js/hash-example.js
@@ -2,19 +2,27 @@
  * Created by jesse on 2018-10-30.
  */
 
+const hello = "Hello, World!";
+
+
+
+//NOTE: solidity hash uses sha3 - keccak 256
 var Web3 = require('web3');
 var web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider());
 
+console.log("web3 - keccak 256 hash, current SHA-3 implementation")
+console.log("hash : " + web3.eth.accounts.hashMessage(hello));
 
-function watchBalance() {
-    var coinbase = web3.eth.coinbase;
-    var originalBalance = web3.eth.getBalance(coinbase).toNumber();
-    document.getElementById('coinbase').innerText = 'coinbase: ' + coinbase;
-    document.getElementById('original').innerText = ' original balance: ' + originalBalance + '    watching...';
-    web3.eth.filter('latest').watch(function() {
-        var currentBalance = web3.eth.getBalance(coinbase).toNumber();
-        document.getElementById("current").innerText = 'current: ' + currentBalance;
-        document.getElementById("diff").innerText = 'diff:    ' + (currentBalance - originalBalance);
-    });
-}
+
+
+
+//NOTE: sodium hash uses BLAKE2b, its an older hashing algorithm. It was an improvement over BLAKE
+//     BLAKE2 is faster then Keccak in software, slower when both using ASICS hardware
+//     It was also a SHA-3 finalist, in the end Keccak won.
+var sodium = require('sodium-native');
+
+var buf1 = Buffer.alloc(sodium.crypto_generichash_BYTES); //== 32 BYTES
+sodium.crypto_generichash(buf1, Buffer.from(hello));
+console.log("\nsodium - BLAKE2b hash algo, it was a SHA-3 finalist")
+console.log("hash : 0x" + buf1.toString('hex'));
